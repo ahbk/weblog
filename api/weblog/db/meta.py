f141a88f392ca4004fb4d902acb31f625fe0f547
Alexander Holmbäck <alexander.holmback@gmail.com>
2023-05-29 19:13:04 +0200

    configure authentication backend

--- api/weblog/db/meta.py
+++ api/weblog/db/meta.py
@@ -2,10 +2,10 @@ from sqlalchemy.ext.asyncio import create_async_engine
 import os
 
 engine = create_async_engine(
-    f"postgresql+asyncpg://postgres:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:5432/weblog",
+    f"postgresql+asyncpg://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:5432/weblog",
     echo=True,
 )
 engine_test = create_async_engine(
-    f"postgresql+asyncpg://postgres:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:5432/weblog_test",
+    f"postgresql+asyncpg://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:5432/weblog_test",
     echo=True,
 )
--- api/weblog/main.py
+++ api/weblog/main.py
@@ -1,16 +1,48 @@
 from typing import Union
 import uvicorn
-import os
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
-from weblog.db.models import Post
-from sqlalchemy import select
+from weblog.db.models import User, create_db_and_tables
+from weblog.schemas import UserCreate, UserRead, UserUpdate
+from weblog.users import auth_backend, current_active_user, fastapi_users
 
 app = FastAPI()
 
-SECRET = os.environ["SECRET_KEY"]
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
 
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+@app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
 
 @app.get("/")
 async def read_root():
