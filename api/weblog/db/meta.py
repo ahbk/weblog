4eda766e5f48da0ad9a835307ef486a3c6e96a85
Oscar Gullberg <ogullberg@gmail.com>
2023-06-05 16:30:06 +0200

    Allow overriding db port from config

--- api/weblog/db/meta.py
+++ api/weblog/db/meta.py
@@ -1,11 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 import os
 
+port = os.getenv("POSTGRES_PORT", "5439")
+
 engine = create_async_engine(
-    f"postgresql+asyncpg://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:5432/weblog",
+    f"postgresql+asyncpg://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:{port}/weblog",
     echo=True,
 )
 engine_test = create_async_engine(
-    f"postgresql+asyncpg://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:5432/weblog_test",
+    f"postgresql+asyncpg://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}:{port}/weblog_test",
     echo=True,
 )
